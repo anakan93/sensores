0fb14d5d4bd2b11209cf5fba5f0364991a80348a
anakan93 <65632803+anakan93@users.noreply.github.com>
2022-01-18 12:11:28 -0500

    organizacion tareas

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #define yellowled D9
 #define redled D10
 #define greenled D11
+#define mainDELAY_LOOP_COUNT  100E6
+
 
 void task_LED1(void *param);
 void task_LED2(void *param);
@@ -13,36 +15,41 @@ void task_LED2(void *param);
 void setup(){
   Serial.begin(9600);
   xTaskCreate(task_LED1,"task1",1024*2,NULL,3,NULL);
-  xTaskCreate(task_LED2,"task1",1024*2,NULL,3,NULL);
- vTaskStartScheduler();
+  xTaskCreate(task_LED2,"task2",1024*2,NULL,3,NULL);
+  vTaskStartScheduler();
 }
-/*void main(){
-  setup();
 
- while (1)
- {
-
- }
-
-}*/
 
 void task_LED1(void *param)
 {
-  while(1)
+  const char *pcTaskName="Task 1 is running";
+  volatile unsigned long ul; 
+  for(;;)
   {
-    const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+    Serial.println( pcTaskName );
+    for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ ) {}
+   /*const TickType_t xDelay = 100 / portTICK_PERIOD_MS;
     printf("this is task 1 ");
-    vTaskDelay( xDelay );
+    vTaskDelay( xDelay );*/
   }
   vTaskDelete(NULL);
 }
 
 void task_LED2(void *param)
 {
-  while(1)
+  const char *pcTaskName="Task 2 is running";
+  volatile unsigned long ul; 
+  for(;;)
   {
-    printf("this is task 2");
+    Serial.println( pcTaskName );
+    for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ ) {}
+    /*const TickType_t xDelay = 100 / portTICK_PERIOD_MS;
+    printf("this is task 2 ");
+     vTaskDelay( xDelay );*/
   }
   vTaskDelete(NULL);
 }
-void loop (){}
+void loop (){
+  
+
+}
